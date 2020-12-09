c57e5a5ccbc15790c12054b103b1240210f631b8
Mark <mark@Marks-MacBook-Air.local>
2020-12-09 17:38:10 -0500

    essentially finished

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import "./App.css";
+import axios from 'axios'
+
+
 
 function App() {
+  // const [nasaDataFromApi, setNasaDataFromApi] = useState({})
+  const [title, setTitle] = useState("")
+  const [date, setDate] = useState("")
+  const [videoURL, setVideoURL] = useState("")
+  const [explanation, setExplanation] = useState("")
+
+
+  useEffect(() => {
+    axios
+      .get (
+        `https://api.nasa.gov/planetary/apod?api_key=QTu3yE2OUyP4BTP1nHE396h5ywBkCP4KyeYh78nK`
+        )
+      .then ( (data) => {
+        // nasaDataFromApi(data.data)
+        setTitle(data.data.title)
+        setDate(data.data.date)
+        setVideoURL(data.data.url)
+        setExplanation(data.data.explanation)
+        // console.log(data.data)
+      })
+    }, []) 
+
+  // console.log(title)
+  // console.log(date)
+  // console.log(videoURL)
+  // console.log(explanation) 
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <div className="content">
+        <h2>{title}</h2>
+        <p>{date}</p>
+        <iframe width="420" height="315" src={videoURL}> </iframe>
+        <p>{explanation}</p>
+      </div>
     </div>
   );
 }
