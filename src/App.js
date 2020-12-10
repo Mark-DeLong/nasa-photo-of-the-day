c58f60ce82b2d0b2e8cde65174fc95c7542ff46d
Mark <mark@Marks-MacBook-Air.local>
2020-12-10 16:22:16 -0500

    made defualt theme

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
 import React, { useState, useEffect} from "react";
 import "./App.css";
-import axios from 'axios'
 import Title from './Title.js'
 import Date from './Date.js'
 import Explanation from './Explanation.js'
-import VideoUrl from './VideoUrl.js'
+import Image from './Image.js'
 
 function App() {
-  const [title, setTitle] = useState("")
-  const [date, setDate] = useState("")
-  const [videoURL, setVideoURL] = useState("")
-  const [explanation, setExplanation] = useState("")
-
-
-  useEffect(() => {
-      axios
-          .get (
-          `https://api.nasa.gov/planetary/apod?api_key=QTu3yE2OUyP4BTP1nHE396h5ywBkCP4KyeYh78nK`
-          )
-          .then ( (data) => {
-              setTitle(data.data.title)
-              setDate(data.data.date)
-              setVideoURL(data.data.url)
-              setExplanation(data.data.explanation)
-          })
-  }, []) 
-
+  
   return (
     <div className="App">
 
@@ -33,7 +14,7 @@ function App() {
         
       <Date />
        
-      <VideoUrl />
+      <Image />
 
       <Explanation />
       
--- src/Image.js
+++ src/Image.js
@@ -2,8 +2,8 @@ import React, { useState, useEffect} from "react";
 import "./App.css";
 import axios from 'axios'
 
-const VideoUrl = () => {
-    const [videoURL, setVideoURL] = useState("")
+const Image = () => {
+    const [image, setImage] = useState("")
 
     useEffect(() => {
         axios
@@ -11,13 +11,14 @@ const VideoUrl = () => {
             `https://api.nasa.gov/planetary/apod?api_key=QTu3yE2OUyP4BTP1nHE396h5ywBkCP4KyeYh78nK`
             )
             .then ( (data) => {
-                setVideoURL(data.data.url)
+                setImage(data.data.url)
             })
     }, []) 
 
     return (
-        <iframe width="420" height="315" src={videoURL} ></iframe>
+        // <iframe width="420" height="315" src={videoURL} ></iframe>
+        <img width="420" height="315" src={image} ></img>
     )
 }
 
-export default VideoUrl
+export default Image
